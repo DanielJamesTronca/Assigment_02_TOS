94ecda3d562b28c76e961a288959fe400f985960
DanielJamesTronca <53578516+DanielJamesTronca@users.noreply.github.com>
2019-12-08 18:25:11 +0100

    Code fixed

--- src/main/java/it/unipd/tos/model/MenuItem.java
+++ src/main/java/it/unipd/tos/model/MenuItem.java
@@ -7,11 +7,6 @@ PANINO, FRITTO, BEVANDA
 ItemType type=ItemType.PANINO;
 String name;
 double price=10;
-public MenuItem(ItemType type, String name, double price) {
-this.type = type;
-this.name = name;
-this.price = price;
-}
 public MenuItem() {
 }
 public double getPrice() {
